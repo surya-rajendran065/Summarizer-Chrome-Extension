4918f9742d43e3eb76770bf7d380dafc77803897
surya-r <rajensur000@esd401.org>
2025-11-19 15:00:59 -0800

    Starting implementing code to record user's voice and storing it
    This feature is not finished

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,7 +1,13 @@
+// Stores audio data in individual chunks for efficiency
+const audioChunks = [];
+
 let startEffect = new Audio(chrome.runtime.getURL("audio/Start.mp3"));
 let stopEffect = new Audio(chrome.runtime.getURL("audio/Stop.mp3"));
-let stream;
 
+// Media Recorder object that listens for audio from user's mic
+let mediaRecorder;
+
+// Reduces the need to type 'navigator.mediaDevices.getUserMedia()' each time
 const getUserMedia = navigator.mediaDevices.getUserMedia.bind(
     navigator.mediaDevices
 );
@@ -11,6 +17,7 @@ function playStartEffect() {
     startEffect.play();
 }
 
+// Plays the sound effect indicating the AI won't listen to more audio
 function playStopEffect() {
     stopEffect.play();
 }
@@ -35,19 +42,50 @@ function stopAIAgentSound() {
     textToSpeech("Exiting AI Agent");
 }
 
-// Listens for audio from user's microphone
-async function getAudioInputMic() {
-    const options = { audio: true };
-    const audioInput = await getUserMedia(options);
-    return audioInput;
+// Stores data in array to be processed later
+function handleAudioData(data) {
+    console.log(data);
+    audioChunks.push(data);
 }
 
-// getAudioInputMic()
-//     .then((result) => {
-//         console.log(`Result finished: ${result}`);
-//         stream = result;
-//         console.log(`Active: ${stream.active}`);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     });
+// Listens for audio from user's microphone
+async function getMediaRecorder() {
+    console.log("Starting Recording...");
+
+    const options = { audio: true };
+
+    const audioInput = await getUserMedia(options);
+
+    const audioStream = await audioInput;
+
+    const localMedia = new MediaRecorder(audioStream);
+
+    return localMedia;
+}
+
+getMediaRecorder().then((result) => {
+    // Sets mediaRecorder to the reuslting MediaRecorder object
+    mediaRecorder = result;
+
+    mediaRecorder.start(1000);
+
+    mediaRecorder.ondataavailable = (event) => {
+        console.log(event.data);
+        audioChunks.push(event.data);
+    };
+
+    // Wait 5 sec before stopping
+    setTimeout(() => {
+        mediaRecorder.stop();
+        setTimeout(() => {
+            const finishedBlob = new Blob(audioChunks, {
+                type: "audio/webm;codecs=opus",
+            });
+            console.log(finishedBlob);
+            // const finishedAudio = new Audio(finishedBlob);
+            // const urlData = URL.createObjectURL(finishedAudio);
+            // console.log(mediaRecorder);
+            // console.log(urlData);
+        }, 1000);
+    }, 5000);
+});
